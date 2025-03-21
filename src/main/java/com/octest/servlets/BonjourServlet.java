9dc0c2acf9425be3dc8599c4a645f539530cd2cf
JSaidi1 <jabrane_saidi@outlook.com>
2025-03-21 10:37:52 +0100

    Gérer les sessions [https://openclassrooms.com/fr/courses/2434016-developpez-des-sites-web-avec-java-ee/2438676-gerer-les-sessions]

--- src/main/java/com/octest/servlets/BonjourServlet.java
+++ src/main/java/com/octest/servlets/BonjourServlet.java
@@ -14,67 +14,24 @@ public class BonjourServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //== If we want deactivate the session:
+        //HttpSession session = request.getSession();
+        //session.invalidate();
+
         this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // On récupère le champ description comme d'habitude
-        String description = request.getParameter("description");
-        request.setAttribute("description", description);
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
 
-        // On récupère le champ du fichier
-        Part part = request.getPart("fichier");
+        HttpSession session = request.getSession();
 
-        // On vérifie qu'on a bien reçu un fichier
-        String nomFichier = getNomFichier(part);
-
-        // Si on a bien un fichier
-        if (nomFichier != null && !nomFichier.isEmpty()) {
-            String nomChamp = part.getName();
-            // Corrige un bug du fonctionnement d'Internet Explorer
-            nomFichier = nomFichier.substring(nomFichier.lastIndexOf('/') + 1)
-                    .substring(nomFichier.lastIndexOf('\\') + 1);
-
-            // On écrit définitivement le fichier sur le disque
-            ecrireFichier(part, nomFichier, CHEMIN_FICHIERS);
-
-            request.setAttribute(nomChamp, nomFichier);
-        }
+        session.setAttribute("nom", nom);
+        session.setAttribute("prenom", prenom);
 
         this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
-    }
-
-    private void ecrireFichier(Part part, String nomFichier, String chemin) throws IOException {
-        BufferedInputStream entree = null;
-        BufferedOutputStream sortie = null;
-        try {
-            entree = new BufferedInputStream(part.getInputStream(), TAILLE_TAMPON);
-            sortie = new BufferedOutputStream(new FileOutputStream(new File(chemin + nomFichier)), TAILLE_TAMPON);
-
-            byte[] tampon = new byte[TAILLE_TAMPON];
-            int longueur;
-            while ((longueur = entree.read(tampon)) > 0) {
-                sortie.write(tampon, 0, longueur);
-            }
-        } finally {
-            try {
-                sortie.close();
-            } catch (IOException ignore) {
-            }
-            try {
-                entree.close();
-            } catch (IOException ignore) {
-            }
-        }
-    }
-
-    private static String getNomFichier(Part part) {
-        for (String contentDisposition : part.getHeader("content-disposition").split(";")) {
-            if (contentDisposition.trim().startsWith("filename")) {
-                return contentDisposition.substring(contentDisposition.indexOf('=') + 1).trim().replace("\"", "");
-            }
-        }
-        return null;
     }
 }
