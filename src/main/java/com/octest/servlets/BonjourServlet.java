2c8c5c3cbe62c2b33dddff224f2e977dede4ca06
JSaidi1 <jabrane_saidi@outlook.com>
2025-03-21 09:35:45 +0100

    Envoyer des fichiers [https://openclassrooms.com/fr/courses/2434016-developpez-des-sites-web-avec-java-ee/2438671-envoyer-des-fichiers]

--- src/main/java/com/octest/servlets/BonjourServlet.java
+++ src/main/java/com/octest/servlets/BonjourServlet.java
@@ -1,24 +1,80 @@
-package com.octest.servlets;    
-    
+package com.octest.servlets;
+
 import java.io.*;
+
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-    
+
 @WebServlet(name = "BonjourServlet", value = "/BonjourServlet")
 public class BonjourServlet extends HttpServlet {
-    
+
+    public static final int TAILLE_TAMPON = 10240;
+    public static final String CHEMIN_FICHIERS = "/home/joel/dataInfo/java/jakarta_ee/others/files/"; // (destination final => A changer)
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String nom = request.getParameter("nom");
+        // On récupère le champ description comme d'habitude
+        String description = request.getParameter("description");
+        request.setAttribute("description", description);
 
-        request.setAttribute("nom", nom);
+        // On récupère le champ du fichier
+        Part part = request.getPart("fichier");
+
+        // On vérifie qu'on a bien reçu un fichier
+        String nomFichier = getNomFichier(part);
+
+        // Si on a bien un fichier
+        if (nomFichier != null && !nomFichier.isEmpty()) {
+            String nomChamp = part.getName();
+            // Corrige un bug du fonctionnement d'Internet Explorer
+            nomFichier = nomFichier.substring(nomFichier.lastIndexOf('/') + 1)
+                    .substring(nomFichier.lastIndexOf('\\') + 1);
+
+            // On écrit définitivement le fichier sur le disque
+            ecrireFichier(part, nomFichier, CHEMIN_FICHIERS);
+
+            request.setAttribute(nomChamp, nomFichier);
+        }
 
         this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    }
+
+    private void ecrireFichier(Part part, String nomFichier, String chemin) throws IOException {
+        BufferedInputStream entree = null;
+        BufferedOutputStream sortie = null;
+        try {
+            entree = new BufferedInputStream(part.getInputStream(), TAILLE_TAMPON);
+            sortie = new BufferedOutputStream(new FileOutputStream(new File(chemin + nomFichier)), TAILLE_TAMPON);
+
+            byte[] tampon = new byte[TAILLE_TAMPON];
+            int longueur;
+            while ((longueur = entree.read(tampon)) > 0) {
+                sortie.write(tampon, 0, longueur);
+            }
+        } finally {
+            try {
+                sortie.close();
+            } catch (IOException ignore) {
+            }
+            try {
+                entree.close();
+            } catch (IOException ignore) {
+            }
+        }
+    }
+
+    private static String getNomFichier(Part part) {
+        for (String contentDisposition : part.getHeader("content-disposition").split(";")) {
+            if (contentDisposition.trim().startsWith("filename")) {
+                return contentDisposition.substring(contentDisposition.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
     }
 }
--- src/main/webapp/WEB-INF/bonjour.jsp
+++ src/main/webapp/WEB-INF/bonjour.jsp
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <title>[bonjour.jsp]</title>
+    <meta charset="utf-8"/>
+    <title>Test</title>
 </head>
 <body>
+    <c:if test="${ !empty fichier }"><p><c:out value="Le fichier ${ fichier } (${ description }) a été uploadé !"/></p>
+    </c:if>
+    <form method="post" action="bonjour" enctype="multipart/form-data">
+        <p>
+            <label for="description">Description du fichier : </label>
+            <input type="text" name="description" id="description"/>
+        </p>
+        <p>
+            <label for="fichier">Fichier à envoyer : </label>
+            <input type="file" name="fichier" id="fichier"/>
+        </p>
 
-  <c:if test="${ !empty nom }"><p><c:out value="Bonjour, vous vous appelez ${ nom }" /></p></c:if>
-
-  <form method="post" action="bonjour">
-    <label for="nom">Nom : </label>
-    <input type="text" name="nom" id="nom" />
-
-    <input type="submit" />
-  </form>
+        <input type="submit"/>
+    </form>
 
 </body>
 </html>
